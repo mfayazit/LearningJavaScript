77fcd853d9d6d98f0e7bd2d3958ea6eea13da158
Muhammad Fayaz <55663300+mfayazit@users.noreply.github.com>
2023-11-03 13:29:57 +0000

    variables

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const accountID = 122334;
+let emailID= 'mfayazit@gmail.com'
+var validPassword = 'abc123'
+empCity = 'Swat'
+let empState;
+
+
+// accountID = 2
+
+console.log(typeof empCity)
+console.log(typeof empState)
+
+console.table([accountID, emailID, validPassword, empCity, empState])
